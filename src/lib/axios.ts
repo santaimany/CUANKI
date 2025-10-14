440c10246446f16165414e008058dbdcef6a45a4
santaimany <santaimany@gmail.com>
2025-10-14 16:45:23 +0700

    Update axios baseURL to use environment variable for better configuration

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://103.186.0.127',
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
